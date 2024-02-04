e015915c72b495a56c53a7a6f082b8f6c35e0a91
ruuushhh <rushikeshtayade2001@gmail.com>
2024-02-04 20:48:43 +0530

    Patterns/star_pyramid-inverted.py

---
+++ Patterns/star_pyramid-inverted.py
@@ -0,0 +1,15 @@
+def pattern(num: int):
+    k=num-1
+    for i in range(0, num):
+        for _ in range(0, i):
+            print(end=" ")
+
+        for _ in range(0, k+1):
+            print('* ', end="")        
+
+        k = k-1
+
+        print()
+
+num = int(input())
+pattern(num)
