b3aa4808d3891aeeeb054b9f77c15d63cb3f26a3
ruuushhh <rushikeshtayade2001@gmail.com>
2024-02-04 16:50:24 +0530

    Basics/reverse_a_number.py

--- Basics/reverse_a_number.py
+++ Basics/reverse_a_number.py
@@ -1,6 +1,9 @@
 def reverse_a_number(n):
-    x = str(n)
-    return int(x[::-1])
+    reverse = 0
+    while n!=0:
+        reverse = reverse*10 + n%10
+        n = n//10
+    return reverse
 
 n = int(input())
 print(reverse_a_number(n))
